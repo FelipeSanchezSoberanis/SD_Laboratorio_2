4d8233237082f74d9abc75b49070704dd179d64c
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2021-04-29 12:44:06 -0500

    Circuito solo con nors

--- Compuertas.vhd
+++ Compuertas.vhd
@@ -10,9 +10,8 @@ end compuertas;
 
 architecture behaviour of compuertas is
 	signal temp1, temp2, temp3, temp4: std_logic;
-	-- signal temp5, temp6, temp7, temp8: std_logic;
-	signal temp9, temp10, temp11, temp12, temp13, temp14, temp15, temp16: std_logic;
-	signal notB, notC, notD: std_logic;
+	signal temp5, temp6, temp7, temp8, temp9, temp10, temp11, temp12, temp13, temp14, temp15: std_logic;
+	signal notA, notB, notC, notD: std_logic;
 	signal BxorC: std_logic;
 	begin
 		temp1 <= (not D) and (not C) and (not A);
@@ -22,34 +21,41 @@ architecture behaviour of compuertas is
 
 		S1 <= temp1 or temp2 or temp3 or temp4;
 
-		notB <= B nand B;
-		notC <= C nand C;
-		notD <= D nand D;
-		BxorC <= (B nand (B nand C)) nand (C nand (B nand C));
+		notA <= A nor A;
+		notB <= B nor b;
+		notC <= C nor C;
+		notD <= D nor D;
 
-		-- temp5 <= (notD) and (notC) and (notA);
-		-- (notD) and (notC) = (notD nand notC) nand (notD nand notC)
-		temp9 <= (notD nand notC) nand (notD nand notC);
-		-- temp9 and (not A) = (temp9 nand (not A)) nand (temp9 nand (not A))
-		temp10 <= (temp9 nand (not A)) nand (temp9 nand (not A));
+		BxorC <= ((B nor B) nor (C nor C)) nor (B nor C);
 
-		-- temp6 <= (notD) and A and (BxorC);
-		-- notD and A = (notD nand A) nand (notD nand A)
-		temp11 <= (notD nand A) nand (notD nand A);
-		-- temp11 and (BxorC) = (temp11 nand BxorC) nand (temp11 nand BxorC)
-		temp12 <= (temp11 nand BxorC) nand (temp11 nand BxorC);
+		-- notD and notC = (notD nor notD) nor (notC nor notC)
+		temp5 <= (notD nor notD) nor (notC nor notC);
+		-- temp5 and notA = (temp5 nor temp5) nor (notA nor notA)
+		temp6 <= (temp5 nor temp5) nor (notA nor notA);
 
-		-- temp7 <= (notD) and C and B;
-		-- (notD) and C = (notD nand C) nand (notD nand C)
-		temp13 <= (notD nand C) nand (notD nand C);
-		-- temp13 and B = (temp13 nand B) nand (temp13 nand B)
-		temp14 <= (temp13 nand B) nand (temp13 nand B);
+		-- notD and A = (notD nor notD) nor (A nor A)
+		temp7 <= (notD nor notD) nor (A nor A);
+		-- temp7 and BxorC = (temp7 nor temp7) nor (BxorC nor BxorC)
+		temp8 <= (temp7 nor temp7) nor (BxorC nor BxorC);
 
-		-- temp8 <= D and (notC) and (notB);
-		-- D and (notC) = (D nand notC) nand (D nand notC)
-		temp15 <= (D nand notC) nand (D nand notC);
-		-- temp15 and (notB) = (temp15 nand notB) nand (temp15 nand notB)
-		temp16 <= (temp15 nand notB) nand (temp15 nand notB);
+		-- notD and C = (notD nor notD) nor (C nor C)
+		temp9 <= (notD nor notD) nor (C nor C);
+		-- temp9 and B = (temp9 nor temp9) nor (B nor B)
+		temp10 <= (temp9 nor temp9) nor (B nor B);
 
-		S2 <= temp10 or temp12 or temp14 or temp16;
+		-- D and notC = (D nor D) nor (notC nor notC)
+		temp11 <= (D nor D) nor (notC nor notC);
+		-- temp11 and notB = (temp11 nor temp11) nor (notB nor notB)
+		temp12 <= (temp11 nor temp11) nor (notB nor notB);
+
+		-- S2 <= temp6 or temp8 or temp10 or temp12;
+
+		-- temp6 or temp8 = (temp6 nor temp8) nor (temp6 nor temp8)
+		temp13 <= (temp6 nor temp8) nor (temp6 nor temp8);
+		-- temp13 or temp10 = (temp13 nor temp10) nor (temp13 nor temp10)
+		temp14 <= (temp13 nor temp10) nor (temp13 nor temp10);
+		-- temp14 or temp12 = (temp14 nor temp12) nor (temp14 nor temp12)
+		temp15 <= (temp14 nor temp12) nor (temp14 nor temp12);
+
+		S2 <= temp15;
 end behaviour;
